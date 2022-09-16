ec4943ba796becc20730e9232d923434b852897c
Mrsschalkwyk <96808682+Mrsschalkwyk@users.noreply.github.com>
2022-09-16 12:43:18 +1000

    updatef

---
+++ calculator/script.js
@@ -0,0 +1,15 @@
+// function add(value) {
+//     let counter = document.getElementById('number')
+//     let newValue = parseInt(counter.innerHTML) + value
+
+//     counter.innerHTML = newValue
+//     console.log("its working", newValue);
+    
+// }
+
+function divideBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = 10;
+document.getElementById("result").innerHTML = num1 / num2;
+}
